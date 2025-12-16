f2ce759637b9f27354660fa5abe6e047f8ecf4d2
Abdulmalik Haruna <princeabdulaliyu@gmail.com>
2025-12-16 15:27:08 +0100

    create models for serialization

--- api/models.py
+++ api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class BugPost(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class BugSolution(models.Model):
+    description = models.TextField()
+    bug_post = models.ForeignKey(BugPost, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    upvotes = models.ManyToManyField(User, related_name='upvoted_posts', blank=True)
+
+class Comment(models.Model):
+    description = models.TextField()
+    bug_solution = models.ForeignKey(BugSolution, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=60, unique=True, blank=True)
---
+++ api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import (
+    BugPost,
+    BugSolution,
+    Comment,
+    Tag,
+)
+
+
+class BugPostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = BugPost
+        fields = '__all__'
+
+class BugSolutionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = BugSolution
+        fields = '__all__'
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class TagSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
