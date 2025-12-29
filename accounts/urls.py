e126fa0406695afa787cc154a89e73c9e81b2997
Abdulmalik Haruna <princeabdulaliyu@gmail.com>
2025-12-29 09:14:32 +0100

    create profile endpoint for viewing and editing profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterView, login, logout
+from .views import RegisterView, login, logout, profile
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
+    path('users/me/', profile, name='profile')
 ]
