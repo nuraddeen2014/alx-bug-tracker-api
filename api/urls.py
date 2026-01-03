c9b07a719f725a285606925b82fb17232e21eff6
Abdulmalik Haruna <princeabdulaliyu@gmail.com>
2026-01-03 10:16:57 +0100

    configure url for homepage

--- api/urls.py
+++ api/urls.py
@@ -16,7 +16,7 @@ router.register(r'tag', TagCreateView)
 
 urlpatterns = [
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-     path('api/', include(router.urls)),
+     path('', include(router.urls)),
 ]
 
 ### Push committed changes to feature origin ### Don't forget to pull the latest changes from main branch###
